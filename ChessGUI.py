516796f860ba833679c4bf1378271d9de3070e62
inform4ar <157527847+inform4ar@users.noreply.github.com>
2024-02-13 10:43:20 -0800

    Update ChessGUI.py

--- ChessGUI.py
+++ ChessGUI.py
@@ -1,5 +1,6 @@
 from graphics import *
 from Pieces import *
+
 #from screeninfo import get_monitors
 
 class ChessGUI:
@@ -11,33 +12,74 @@ class ChessGUI:
         self.win.setCoords(0, 0, 1470 - 1, 890 - 1)
         self.win.setBackground("grey16")
 
-
     def draw_piece(self, piece):
-        x = piece.getX() * 100 + 500
+        x = piece.getX() * 100 + 498
         y = piece.getY() * 100 - 5
         IMG_file = piece.getIMG_file()
 
         IMG = Image(Point(x, y), IMG_file)
+        piece.setIMG_obj(IMG)
         IMG.draw(self.win)
 
-    def draw_squares(self):
-        y = -5
-        for j in range(1, 9):
-            y += 100
-            x = 500
-            for i in range(1, 9):
-                x += 100
-                sqr = Rectangle(Point(x - 50, y - 50), Point(x + 50, y + 50))
-                sqr.setWidth(0)
-                if i % 2 == 0:
-                    if j % 2 == 0:
-                        sqr.setFill("PaleGreen4")
-                    else:
-                        sqr.setFill("light goldenrod yellow")
+    def undraw_piece(self, piece):
+        IMG = piece.getIMG_obj()
+        IMG.undraw()
+
+    def draw_squares(self, board):
+        for square in board.state:
+            x = square.getX() * 100 + 500
+            y = square.getY() * 100 - 5
+
+            sqr = Rectangle(Point(x - 50, y - 50), Point(x + 50, y + 50))
+            sqr.setWidth(0)
+            if square.getX() % 2 == 0:
+                if square.getY() % 2 == 0:
+                    sqr.setFill("PaleGreen4")
                 else:
-                    if j % 2 == 0:
-                        sqr.setFill("light goldenrod yellow")
-                    else:
-                        sqr.setFill("PaleGreen4")
+                    sqr.setFill("light goldenrod yellow")
+            else:
+                if square.getY() % 2 == 0:
+                    sqr.setFill("light goldenrod yellow")
+                else:
+                    sqr.setFill("PaleGreen4")
                         
-                sqr.draw(self.win)
+            sqr.draw(self.win)
+
+    def getMouse(self):
+        return self.win.getMouse()
+
+    def check_square(self, pt, board):
+        for square in board.state:
+            x = square.getX() * 100 + 500
+            y = square.getY() * 100 - 5
+            
+            if pt.getX() > (x - 50) and pt.getX() < (x + 50):
+                if pt.getY() > (y - 50) and pt.getY() < (y + 50):    
+                    return square
+
+    def change_sqr_color(self, squares, color1, color2):
+        for square in squares:
+            x = square.getX() * 100 + 500
+            y = square.getY() * 100 - 5
+
+            sqr = Rectangle(Point(x - 50, y - 50), Point(x + 50, y + 50))
+            sqr.setWidth(0)
+                
+            if square.getX() % 2 == 0:
+                if square.getY() % 2 == 0:
+                    sqr.setFill(color1)
+                else:
+                    sqr.setFill(color2)
+            else:
+                if square.getY() % 2 == 0:
+                    sqr.setFill(color2)
+                else:
+                    sqr.setFill(color1)
+                    
+            sqr.draw(self.win)
+
+            p = square.get_piece()
+            if p != None:
+                self.undraw_piece(p)
+                self.draw_piece(p)
+                
